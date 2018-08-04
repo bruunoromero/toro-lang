f9494e912432134d0a7ffd7999e5a83bdc972aa7
brunoromero <brunoromero172@hotmail.com>
2018-08-04 15:34:50 -0300

    chore: configured test

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.(ts)$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsConfigFile: "tsconfig.json",
+    },
+  },
+  testMatch: ["**/__tests__/*.+(ts|js)"],
+};
