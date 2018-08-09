35ec0c8f970888d64730278320d3c632c8da0997
brunoromero <brunoromero172@hotmail.com>
2018-08-09 18:20:25 -0300

    feat: implemented parsers for block and definition

--- __tests__/parser.spec.ts
+++ __tests__/parser.spec.ts
@@ -2,10 +2,25 @@ import { parse } from "../src/parser";
 import { pprint } from "../src/utils/print";
 
 describe("Import", () => {
-  it("Should parse", () => {
+  it("Should parse imports", () => {
     const sample = `
 import List
 import List.Core
+`;
+
+    expect(() => {
+      const result = parse(sample);
+    }).not.toThrow();
+  });
+});
+
+describe("Definition", () => {
+  it("Should parse definitions", () => {
+    const sample = `
+def main = {
+  def t = {}
+}
+export def teste = {}
 `;
 
     expect(() => {
--- src/grammar.ne
+++ src/grammar.ne
@@ -6,9 +6,17 @@ const lexer = require('./lexer.js');
 
 @lexer lexer
 
-program -> _ delimited[import, %NL] _
+program -> _ delimited[import, %NL]:? _ delimited[exportableDefinition, %NL]:? _
 
 import -> _ "import" __ delimited[%IDENTIFIER, %DOT] %WS:?
+
+exportableDefinition -> (_ "export"):? definition
+
+definition -> _ "def" __ %IDENTIFIER _ "=" block
+
+block -> _ "{" _ delimited[expression, %NL]:? _ "}" %WS:?
+
+expression -> _ definition _
 
 _ -> (%WS | %NL):*
 __ -> %WS:+
