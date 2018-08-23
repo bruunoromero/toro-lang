5e5ffa3b951215e5d5a1a693c48d01c5b4960fd3
Bruno Romero <brunoromero172@hotmail.com>
2018-08-23 16:15:05 -0300

    refactor: parser

--- src/parser/grammar.ts
+++ src/parser/grammar.ts
@@ -1,12 +1,12 @@
-import { KEYWORDS } from "./keywords";
 import * as R from "ramda";
 import * as P from "parsimmon";
 
 import { TOKENS } from "./tokens";
 import { COMMON } from "./common";
 import { CLAUSES } from "./clauses";
-import { OPERATORS } from "./operators";
+import { KEYWORDS } from "./keywords";
 import { SPECIALS } from "./specials";
+import { OPERATORS } from "./operators";
 
 export const Grammar = P.createLanguage(
   R.mergeAll([OPERATORS, SPECIALS, KEYWORDS, TOKENS, COMMON, CLAUSES]),
